ef45c360d7e07b8efd153d954fa7a5b802e18b05
coalfocks <coalfocks@gmail.com>
2018-04-09 18:56:33 -0600

    killer DT accuracy on bitcoin dataset

--- DTsrc/dt_oracle.py
+++ DTsrc/dt_oracle.py
@@ -6,31 +6,25 @@ from sklearn.metrics import accuracy_score
 from sklearn import tree
 
 str_to_int = {
-    'young': 0,
-    'pre-presbyopic': 1,
-    'presbyopic': 2,
-    'myope': 0,
-    'hypermetrope': 1,
-    'no': 0,
-    'yes': 1,
-    'reduced': 0,
-    'normal': 1,
-    'soft': 0,
-    'hard': 1,
-    'none': 2
-
+    'buy': 0.0,
+    'sell': 1.0,
+    'hold': 2.0,
+    'None': 0.0
 }
 
-headers = ['age','specs','astigmatism','tear-rate','lenses']
-data = pd.read_csv('lenses.csv')#, header=headers)
+headers = ['Timestamp', 'Open', 'High', 'Low', 'Price', 'Volume', '24hr', 'Action']
+
+data = pd.read_csv('../bitcoin_data/bitcoin.csv')#, header=headers)
 data = data.replace(str_to_int).sample(frac=1)
-X = data.values[:,:3]
-Y = data.values[:,4]
+data = data.convert_objects(convert_numeric=True)
+X = np.nan_to_num(data.values[:,:6])
+Y = np.nan_to_num(data.values[:,7])
 
 X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.6, random_state = 100)
 
 clf_entropy = DecisionTreeClassifier(criterion = "entropy", random_state = 100,
  max_depth=3, min_samples_leaf=5)
+
 clf_entropy.fit(X_train, y_train)
 
 y_pred_en = clf_entropy.predict(X_test)
